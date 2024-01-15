f43b4fc3701862399a40eb0a4c8b468e40397e32
RyanHu021 <ryanhu021@gmail.com>
2024-01-15 15:34:38 -0800

    a

--- schoolsearch.py
+++ schoolsearch.py
@@ -1,5 +1,17 @@
+STUDENTS_FILE = "students.txt"
+GRADES = 7
+
 class Student:
-  def __init__(self, st_last_name, st_first_name, grade, classroom, bus, gpa, t_last_name, t_first_name):
+  st_last_name: str
+  st_first_name: str
+  grade: str
+  classroom: str
+  bus: str
+  gpa: str
+  t_last_name: str
+  t_first_name: str
+
+  def __init__(self, st_last_name: str, st_first_name: str, grade: str, classroom: str, bus: str, gpa: str, t_last_name: str, t_first_name: str):
     self.st_last_name = st_last_name
     self.st_first_name = st_first_name
     self.grade = grade
@@ -10,44 +22,88 @@ class Student:
     self.t_first_name = t_first_name
 
 def query_student(students: list, last_name: str, bus: bool = False):
-  if bus:
-    for student in students:
-      if student.st_last_name == last_name:
-        print(student.st_last_name + " " + student.st_first_name + " " + student.bus)
-  else:
-    for student in students:
-      if student.st_last_name == last_name:
-        print(student.st_last_name + " " + student.st_first_name + " " + student.grade + " " + student.classroom + " " + student.t_last_name + " " + student.t_first_name)
+  for student in students:
+    if student.st_last_name == last_name:
+      if bus:
+        print(f"{student.st_last_name},{student.st_first_name},{student.bus}")
+      else:
+        print(f"{student.st_last_name},{student.st_first_name},{student.grade},{student.classroom},{student.t_last_name},{student.t_first_name}")
 
 def query_teacher(students: list, last_name: str):
   for student in students:
     if student.t_last_name == last_name:
-      print(student.st_last_name + " " + student.st_first_name)
+      print(f"{student.st_last_name},{student.st_first_name}")
 
+def query_grade(students: list, grade: str, high: bool = False, low: bool = False):
+  if high:
+    student = max((student for student in students if student.grade == grade), key=lambda student: student.gpa)
+    print(f"{student.st_last_name},{student.st_first_name},{student.gpa},{student.t_last_name},{student.t_first_name},{student.bus}")
+  elif low:
+    student = min((student for student in students if student.grade == grade), key=lambda student: student.gpa)
+    print(f"{student.st_last_name},{student.st_first_name},{student.gpa},{student.t_last_name},{student.t_first_name},{student.bus}")
+  else:
+    for student in students:
+      if student.grade == grade:
+        print(f"{student.st_last_name},{student.st_first_name}")
+
+def query_bus(students: list, bus: str):
+  for student in students:
+    if student.bus == bus:
+        print(f"{student.st_last_name},{student.st_first_name},{student.grade},{student.classroom}")
+
+def query_average(students: list, grade: str):
+  students_in_grade = [student for student in students if student.grade == grade]
+  average = sum(student.gpa for student in students_in_grade) / len(students_in_grade)
+  print(f"{grade},{average}")
+
+def query_info(students: list):
+  students_by_grade = [[student for student in students if student.grade == grade] for grade in range(GRADES)]
+  for grade in range(GRADES):
+    print(f"Grade {grade}: {len(students_by_grade[grade])}")
+
+def query_quit():
+  print("Exiting...")
+  quit()
+
+def parse_query(students: list, query: str):
+  tokens = query.strip().split()
+  if len(tokens) >= 2 and tokens[0] in ["S", "Student"]:
+    if len(tokens) >= 3 and tokens[2] in ["B", "Bus"]:
+      query_student(students, tokens[1], True)
+    else:
+      query_student(students, tokens[1])
+  elif len(tokens) >= 2 and tokens[0] in ["T", "Teacher"]:
+    query_teacher(students, tokens[1])
+  elif len(tokens) >= 2 and tokens[0] in ["G", "Grade"]:
+    if len(tokens) >= 3 and tokens[2] in ["H", "High"]:
+      query_grade(students, tokens[1], high=True)
+    elif len(tokens) >= 3 and tokens[2] in ["L", "Low"]:
+      query_grade(students, tokens[1], low=True)
+    else:
+      query_grade(students, tokens[1])
+  elif len(tokens) >= 2 and tokens[0] in ["B", "Bus"]:
+    query_bus(students, tokens[1])
+  elif len(tokens) >= 2 and tokens[0] in ["A", "Average"]:
+    query_average(students, tokens[1])
+  elif len(tokens) >= 1 and tokens[0] in ["I", "Info"]:
+    query_info(students)
+  elif len(tokens) >= 1 and tokens[0] in ["Q", "Quit"]:
+    query_quit()
+  else:
+    print("Invalid query")
 
 def main():
-  print("Opening students.txt...")
+  print(f"Opening {STUDENTS_FILE}...")
   try:
-    file = open("students.txt", "r")
-    students = []
-    for line in file:
-      line = line.split(",")
-      students.append(Student(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7]))
-    print("Loaded students.txt.")
+    with open(STUDENTS_FILE, "r") as file:
+      students = []
+      for line in file:
+        line = line.strip().split(',')
+        students.append(Student(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7]))
+    print("Loaded students.txt")
     while True:
-      query = input("Enter a query: ")
-      tokens = query.split(" ")
-      if len(tokens) == 1 and tokens[0] in ["Q", "Quit"]:
-        print("Exiting...")
-        return
-      elif len(tokens) >= 2 and tokens[0] in ["S", "Student"]:
-        if len(tokens) >= 3 and tokens[2] in ["B", "Bus"]:
-          query_student(students, tokens[1], True)
-        else:
-          query_student(students, tokens[1])
-      else:
-        print("Invalid query.")
-    
+      query = input("\nEnter a query: ")
+      parse_query(students, query)
   except IOError:
     print("Unable to find students.txt")
     return
