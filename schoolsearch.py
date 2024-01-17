4b58a465a970c0962ca0bfc81218826e3b4f0e55
RyanHu021 <ryanhu021@gmail.com>
2024-01-16 20:20:13 -0800

    revise tests and add output

--- schoolsearch.py
+++ schoolsearch.py
@@ -1,3 +1,5 @@
+import sys
+
 STUDENTS_FILE = "students.txt"
 GRADES = 7
 MAX_GPA = 4.0
@@ -43,32 +45,32 @@ def query_teacher(students: list, last_name: str):
 
 def query_grade(students: list, grade: str, high: bool = False, low: bool = False):
     try:
-      int(grade)
-      if int(grade) not in range(GRADES):
-          raise ValueError
-      if high:
-        student = max((student for student in students if student.grade ==
-                      grade), key=lambda student: student.gpa)
-        print(f"{student.st_last_name},{student.st_first_name},{student.gpa},{student.t_last_name},{student.t_first_name},{student.bus}")
-      elif low:
-          student = min((student for student in students if student.grade ==
-                        grade), key=lambda student: student.gpa)
-          print(f"{student.st_last_name},{student.st_first_name},{student.gpa},{student.t_last_name},{student.t_first_name},{student.bus}")
-      else:
-          for student in students:
-              if student.grade == grade:
-                  print(f"{student.st_last_name},{student.st_first_name}")
+        int(grade)
+        if int(grade) not in range(GRADES):
+            raise ValueError
+        if high:
+            student = max((student for student in students if student.grade ==
+                          grade), key=lambda student: student.gpa)
+            print(f"{student.st_last_name},{student.st_first_name},{student.gpa},{student.t_last_name},{student.t_first_name},{student.bus}")
+        elif low:
+            student = min((student for student in students if student.grade ==
+                          grade), key=lambda student: student.gpa)
+            print(f"{student.st_last_name},{student.st_first_name},{student.gpa},{student.t_last_name},{student.t_first_name},{student.bus}")
+        else:
+            for student in students:
+                if student.grade == grade:
+                    print(f"{student.st_last_name},{student.st_first_name}")
     except ValueError:
-        print("Invalid Grade")    
+        print("Invalid Grade")
 
 
 def query_bus(students: list, bus: str):
     try:
-      int(bus)
-      for student in students:
-        if student.bus == bus:
-            print(
-                f"{student.st_last_name},{student.st_first_name},{student.grade},{student.classroom}")
+        int(bus)
+        for student in students:
+            if student.bus == bus:
+                print(
+                    f"{student.st_last_name},{student.st_first_name},{student.grade},{student.classroom}")
     except ValueError:
         print("Invalid Bus Route")
 
@@ -78,24 +80,24 @@ def query_average(students: list, grade: str):
         int(grade)
         if int(grade) not in range(GRADES):
             raise ValueError
-        students_in_grade = [
-        student for student in students if student.grade == grade]
-        if len(students_in_grade) != 0:
-            average = round(sum(student.gpa for student in students_in_grade) / \
-            len(students_in_grade),2)
+        if students_in_grade := [
+            student for student in students if student.grade == grade
+        ]:
+            average = round(sum(float(student.gpa) for student in students_in_grade) /
+                            len(students_in_grade), 2)
             print(f"{grade},{average}")
         else:
             print("No students in grade")
-     
-    except ValueError:
+
+    except ValueError as e:
         print("Invalid Grade")
-    
 
 
 def query_info(students: list):
     # count the number of students in each grade and print it
     for grade in range(GRADES):
-        print(f"Grade {grade}: {len([student for student in students if student.grade == str(grade)])}")
+        print(
+            f"{grade}: {len([student for student in students if student.grade == str(grade)])}")
 
 
 def query_quit():
@@ -115,7 +117,7 @@ def parse_query(students: list, query: str):
         query_grade(students, tokens[1])
     elif len(tokens) == 3 and tokens[0] in ["G:", "Grade:"] and tokens[2] in ["H", "High"]:
         query_grade(students, tokens[1], high=True)
-    elif len(tokens) == 3 and tokens[0] in ["G", "Grade"] and tokens[2] in ["L", "Low"]:
+    elif len(tokens) == 3 and tokens[0] in ["G:", "Grade:"] and tokens[2] in ["L", "Low"]:
         query_grade(students, tokens[1], low=True)
     elif len(tokens) == 2 and tokens[0] in ["B:", "Bus:"]:
         query_bus(students, tokens[1])
@@ -149,21 +151,35 @@ def check_line(tokens: list) -> bool:
         return False
     return True
 
-def main():
+
+def main(test_file: str = None):
     print(f"Opening {STUDENTS_FILE}...")
     try:
         with open(STUDENTS_FILE, "r") as file:
             students = []
             for [i, line] in enumerate(file):
-                line = line.strip().split(',')
-                if not check_line(line):
+                tokens = line.strip().split(',')
+                if not check_line(tokens):
                     print(f"Invalid format on line {i + 1}")
                     return
                 students.append(
-                    Student(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7]))
+                    Student(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7]))
         print("Loaded students.txt")
+        if test_file:
+            print(f"Using test suite: {test_file}")
+            try:
+                with open(test_file, "r") as file:
+                    for [i, line] in enumerate(file):
+                        if line.strip() == "" or line.strip().startswith("//"):
+                            continue
+                        print(f"\nEnter a query: {line.strip()}")
+                        parse_query(students, line.strip())
+            except IOError:
+                print(f"Unable to find {test_file}")
+                return
+            return
         while True:
-            query = input("\nEnter a query: ")
+            query = input("\nEnter a query: ").strip()
             parse_query(students, query)
     except IOError:
         print("Unable to find students.txt")
@@ -171,4 +187,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 1:
+        main()
+    elif len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print("Invalid number of arguments")
